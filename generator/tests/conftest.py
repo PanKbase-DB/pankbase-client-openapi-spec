9e65d5218c516ffce8c36e7e3c7714c11f78715e
Keenan Graham <ksgraham@stanford.edu>
2024-08-14 13:54:43 -0700

    Add test

--- generator/tests/conftest.py
+++ generator/tests/conftest.py
@@ -15,3 +15,10 @@ def raw_embedded_fields():
     with open('./generator/tests/rawembedded.json') as f:
         raw_embedded_fields = json.load(f)
     return raw_embedded_fields    
+
+
+@pytest.fixture
+def collection_names():
+    with open('./generator/tests/collection_names.json') as f:
+        collection_names = json.load(f)
+    return collection_names
